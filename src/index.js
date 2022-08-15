e1bea23163e2f912a668304e49dc30353703bc56
Varden41 <arturpavlenko14@gmail.com>
2022-08-15 13:59:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-const postIdToDelete = 1;
+const fetchUsers = async () => {
+  const baseUrl = 'https://jsonplaceholder.typicode.com';
+  const userIds = [1, 2, 3];
 
-fetch(`https://jsonplaceholder.typicode.com/posts/${postIdToDelete}`, {
-  method: 'DELETE',
-})
-  .then(() => console.log('Post deleted'))
-  .catch(error => console.log('Error:', error));
+  // 1. Создаём массив промисов
+  const arrayOfPromises = userIds.map(async userId => {
+    const response = await fetch(`${baseUrl}/users/${userId}`);
+    return response.json();
+  });
+
+  // 2. Запускаем все промисы параллельно и ждем их завершения
+  const users = await Promise.all(arrayOfPromises);
+  console.log(users);
+};
+
+fetchUsers();
