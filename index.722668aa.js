67d87b8a3cab65734d7cd01aa484ce8fc787432c
Varden41 <105563413+Varden41@users.noreply.github.com>
2022-08-16 07:03:18 +0000

    Deploying to gh-pages from @ Varden41/jsTest@99ee987e5820679b3179b2468a3720ded3c638e6 🚀

---
+++ index.722668aa.js
@@ -0,0 +1,2 @@
+var o,t;o=9,t=5,console.log((o+t).toString(2)),(o+t).toString(2);
+//# sourceMappingURL=index.722668aa.js.map
---
+++ index.c772bba9.js
@@ -0,0 +1,2 @@
+!function(){var o,n;o=9,n=5,console.log((o+n).toString(2)),(o+n).toString(2)}();
+//# sourceMappingURL=index.c772bba9.js.map
